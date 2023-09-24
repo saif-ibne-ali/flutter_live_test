1a9c6cab6e4d5997a558a62f6673a9dc5c88495e
saif-ibne-ali <si87868@gmail.com>
2023-09-24 23:43:37 +0600

    module7_live_test

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# module6_live_test
+# module7_live_test
 
-Create a single page that displays a list of items.
+## Question
+Create a Flutter screen that initially displays a count of 0. The screen should have two buttons: one labeled "+" and the other labeled "-".
 
-![Alt text](Screenshot_20230919-001253_flutter_live_test.png)
-
-## Requirements:
-An app bar titled "My Shopping List.", with a cart icon.
-A list of at least 5 shopping items displayed using ListTile widgets.
-Each item should have an icon (use any icon) and a name (name of any object). Apply suitable styling for visual appeal.
+### Requirements:
+Tapping the "+" button should increase the count by 1.
+Tapping the "-" button should decrease the count by 1, but the count should not go below 0.
+If the count reaches 5 or more, a dialog should appear with the message: "Button pressed {count} times."
--- lib/main.dart
+++ lib/main.dart
@@ -7,117 +7,80 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Live Test',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(title: 'Counter App'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My Shopping List"),
-        centerTitle: true,
-        actions: const [
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Icon(
-              Icons.shopping_cart_sharp,
-              size: 25,
-            ),
-          ),
-        ],
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              ListTile(
-                title: Text(
-                  "Apples",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Count:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        _incrementCounter();
+                        if (_counter >= 5) {}
+                      },
+                      child: const Icon(Icons.add)),
                 ),
-                leading: Icon(
-                  Icons.apple,
-                  size: 30,
+                Container(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ElevatedButton(
+                      onPressed: _decrementCounter,
+                      child: const Icon(Icons.remove)),
                 ),
-              ),
-              Divider(
-                thickness: 2,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  "Watch",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-                leading: Icon(
-                  Icons.watch,
-                  size: 30,
-                ),
-              ),
-              Divider(
-                thickness: 2,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  "Face Mask",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-                leading: Icon(
-                  Icons.masks,
-                  size: 30,
-                ),
-              ),
-              Divider(
-                thickness: 2,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  "School Bag",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-                leading: Icon(
-                  Icons.shopping_bag_sharp,
-                  size: 30,
-                ),
-              ),
-              Divider(
-                thickness: 2,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  "Books",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-                leading: Icon(
-                  Icons.book,
-                  size: 30,
-                ),
-              ),
-              Divider(
-                thickness: 2,
-                height: 10,
-              ),
-            ],
-          ),
+              ],
+            )
+          ],
         ),
       ),
     );
